bd8bf6bb02969c3a444937a9197d371675e29cbd
Trung04122003 <lethaitrung03@gmail.com>
2025-12-24 09:21:54 +0700

    PHASE 1: Foundation & Core Setup ⚙️ - Week 1: Project Infrastructure

---
+++ zodiac/src/main/java/com/jci/zodiac/ZodiacSignHrBeApplication.java
@@ -0,0 +1,13 @@
+package com.jci.zodiac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacSignHrBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacSignHrBeApplication.class, args);
+	}
+
+}
---
+++ zodiac/src/test/java/com/jci/zodiac/ZodiacSignHrBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jci.zodiac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiacSignHrBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
